5a715c02edacb7596dab67d0db8cf9731b30bd0e
YuviPanda <yuvipanda@gmail.com>
2013-03-07 07:55:20 +0530

    Generate just the data csvs, not limn config

--- mobile-app-stats.py
+++ mobile-app-stats.py
@@ -1,8 +1,8 @@
 import os
-import os.path
 import MySQLdb as mysql
 from jinja2 import Template
-import limnpy
+#import limnpy
+import unicodecsv as csv
 
 #conn = mysql.connect("s1-analytics-slave.eqiad.wmnet", "research", os.environ["RESEARCH_PASSWORD"], "log")
 conn = mysql.connect("s1-analytics-slave.eqiad.wmnet", read_default_file=os.path.expanduser('~/.my.cnf.research'), db="log")
@@ -114,14 +114,15 @@ graphs = {
         }
 
 
+
 for key, value in graphs.items():
     title = value['title']
     sql = render(value['sql'], sql_env)
     rows = execute(sql)
-    ds = limnpy.DataSource(limn_id=key, limn_name=key, data=list(rows), labels=value['headers'], date_key='Date')
-    ds.write(basedir='.')
-    ds.write_graph(basedir='.')
-    #writer = csv.writer(open(key + ".csv", "w"))
-    #writer.writerow(value['headers'])
-    #for row in rows:
-        #writer.writerow(row)
+    #ds = limnpy.DataSource(limn_id=key, limn_name=key, data=list(rows), labels=value['headers'], date_key='Date')
+    #ds.write(basedir='.')
+    #ds.write_graph(basedir='.')
+    writer = csv.writer(open("datafiles/" + key + ".csv", "w"))
+    writer.writerow(value['headers'])
+    for row in rows:
+        writer.writerow(row)
