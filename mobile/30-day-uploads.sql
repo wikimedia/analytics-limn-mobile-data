5c82384021d5dd9b242418c57ecdddc3be7ec9b5
Yuvi Panda <yuvipanda@gmail.com>
2013-03-15 09:23:17 +0000

    Added 30 day rolling cumulative uploads count

---
+++ mobile/30-day-uploads.sql
@@ -0,0 +1,49 @@
+SELECT
+    Date,
+
+    (
+        (SELECT COUNT(*) FROM {{ tables.upload_attempts }} WHERE
+            event_result = 'success' AND
+            wiki = 'commonswiki' AND
+            DATE(timestamp) < Date AND
+            (timestamp + INTERVAL 0 DAY) > (Date - INTERVAL {{ intervals.running_average }} DAY)
+        ) +
+        (SELECT COUNT(*) FROM {{ tables.upload_web }} WHERE
+            event_action = 'success' AND
+            wiki != 'testwiki' AND
+            DATE(timestamp) < Date AND
+            (timestamp + INTERVAL 0 DAY) > (Date - INTERVAL {{ intervals.running_average }} DAY)
+        )
+    ) AS Total,
+
+    (SELECT COUNT(*) FROM {{ tables.upload_attempts }} WHERE
+        event_platform LIKE 'Android%' AND
+        event_result = 'success' AND
+        wiki = 'commonswiki' AND
+        DATE(timestamp) < Date AND
+        (timestamp + INTERVAL 0 DAY) > (Date - INTERVAL {{ intervals.running_average }} DAY)
+    ) AS Android,
+
+    (SELECT COUNT(*) FROM {{ tables.upload_attempts }} WHERE
+        event_platform LIKE 'iOS%' AND
+        event_result = 'success' AND
+        wiki = 'commonswiki' AND
+        DATE(timestamp) < Date AND
+        (timestamp + INTERVAL 0 DAY) > (Date - INTERVAL {{ intervals.running_average }} DAY)
+    ) AS iOS,
+
+    (SELECT COUNT(*) FROM {{ tables.upload_web }} WHERE
+        event_action = 'success' AND
+        wiki != 'testwiki' AND
+        DATE(timestamp) < Date AND
+        (timestamp + INTERVAL 0 DAY) > (Date - INTERVAL {{ intervals.running_average }} DAY)
+    ) AS Web
+
+-- http://stackoverflow.com/a/6871220/365238
+FROM (
+    SELECT DATE_FORMAT(
+        ADDDATE(CURDATE() - INTERVAL {{ intervals.running_average }} - 1 DAY, @num:=@num+1),
+        '%Y-%m-%d'
+    ) AS Date
+    FROM {{ tables.upload_attempts }}, (SELECT @num:=-1) num LIMIT {{ intervals.running_average }}
+) AS Month;
