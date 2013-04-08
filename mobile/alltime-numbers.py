d536db039025722983b60fd0ee8f8196ceb7e93e
YuviPanda <yuvipanda@gmail.com>
2013-04-08 07:41:26 +0530

    Added script to generate all time upload & survival numbers
    Change-Id: Ibd6f493f3ccc1f9fe7a9bdf07150a038db64862b

---
+++ mobile/alltime-numbers.py
@@ -0,0 +1,62 @@
+uploads_count_android_sql = u"""
+SELECT  count(*) AS uploads
+FROM    {{tables.upload_attempts}}
+WHERE   event_platform LIKE 'Android%'
+        AND event_result="success"
+        AND wiki="commonswiki"
+"""
+
+uploads_count_ios_sql = u"""
+SELECT  count(*) AS uploads
+FROM    {{tables.upload_attempts}}
+WHERE   event_platform LIKE 'iOS%'
+        AND event_result="success"
+        AND wiki="commonswiki"
+"""
+
+uploads_count_web_sql = u"""
+SELECT  count(*) AS uploads
+FROM    {{tables.upload_web}}
+WHERE   event_action="success"
+        AND wiki != "testwiki"
+"""
+
+retained_uploads_count_sql = u"""
+SELECT  count(*)
+FROM    categorylinks
+WHERE   cl_to = %s
+"""
+
+headers = ["source", "total", "undeleted", "deletion percentage"]
+
+
+def results_for(dg, sql, source, category):
+    commons = dg.conn_for('commons')
+    el = dg.conn_for('el')
+
+    all_cursor = el.cursor()
+    all_cursor.execute(dg.render(sql))
+
+    all_uploads = all_cursor.fetchone()
+
+    existing_cursor = commons.cursor()
+    existing_cursor.execute(retained_uploads_count_sql, category)
+
+    existing_uploads = existing_cursor.fetchone()
+
+    total_uploads = all_uploads[0]
+    undeleted_uploads = existing_uploads[0]
+    deleted_uploads = total_uploads - undeleted_uploads
+    deletion_percentage = float(deleted_uploads) / float(total_uploads)
+
+    return [source, total_uploads, undeleted_uploads, '%.2f' % deletion_percentage]
+
+
+def execute(dg):
+
+    results = []
+    results.append(results_for(dg, uploads_count_android_sql, "Android", "Uploaded_with_Mobile/Android"))
+    results.append(results_for(dg, uploads_count_ios_sql, "iOS", "Uploaded_with_Mobile/iOS"))
+    results.append(results_for(dg, uploads_count_web_sql, "Web", "Uploaded_with_Mobile/Web"))
+
+    return (headers, results)
