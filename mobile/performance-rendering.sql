38113444504b7f5df1b280ada21ed45506f957c0
Juliusz Gonera <jgonera@wikimedia.org>
2013-04-23 17:15:09 -0700

    Add a 30-day moving average of the rendering time graph
    The graph will be smoother when we have data for more than one month.
    
    Change-Id: I75bb9a63a7113bec066845ea999477f030514ab4

--- mobile/performance-rendering.sql
+++ mobile/performance-rendering.sql
@@ -1,8 +1,22 @@
 SELECT
-    DATE(timestamp) AS Date,
-    AVG(event_rendering) AS Rendering
-FROM {{ tables.navigation_timing }}
-WHERE
-    event_mobileMode = 'stable' AND
-    DATE(timestamp) >= (CURDATE() - INTERVAL {{ intervals.running_average }} DAY)
-GROUP BY Date;
+    Date,
+
+    (SELECT AVG(event_rendering) FROM {{ tables.navigation_timing }} WHERE
+        event_mobileMode = 'stable' AND
+        DATE(timestamp) < Date AND
+        (timestamp + INTERVAL 0 DAY) > (Date - INTERVAL {{ intervals.running_average }} DAY)
+    ) AS Rendering30Day,
+
+    (SELECT AVG(event_rendering) FROM {{ tables.navigation_timing }} WHERE
+        event_mobileMode = 'stable' AND
+        DATE(timestamp) = Date
+    ) AS Rendering
+
+-- http://stackoverflow.com/a/6871220/365238
+FROM (
+    SELECT DATE_FORMAT(
+        ADDDATE(CURDATE() - INTERVAL {{ intervals.running_average }} - 1 DAY, @num:=@num+1),
+        '%Y-%m-%d'
+    ) AS Date
+    FROM {{ tables.upload_attempts }}, (SELECT @num:=-1) num LIMIT {{ intervals.running_average }}
+) AS Month;
