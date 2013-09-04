ddfadaea51a8cf13358b635a686dd76089c3cebe
Juliusz Gonera <jgonera@wikimedia.org>
2013-09-04 15:28:35 -0700

    Story 1124: Add Edits monthly graphs
    Change-Id: Ie4ff6241e95f13f2081ee6fd313f1aa2910150f3

---
+++ mobile/edits-monthly-successful.sql
@@ -0,0 +1,33 @@
+SELECT
+    DATE_FORMAT(CONCAT(Month.month, '01'), '%Y-%m-%d') AS Month,
+    COALESCE(Main.count, 0) AS Main,
+    COALESCE(Other.count, 0) AS Other
+
+-- http://stackoverflow.com/a/6871220/365238
+FROM (
+    SELECT EXTRACT(YEAR_MONTH FROM SUBDATE(CURDATE(), INTERVAL @num:=@num+1 MONTH)) AS month
+    FROM information_schema.columns, (SELECT @num:=-1) num LIMIT 12
+) AS Month
+LEFT JOIN (
+    SELECT
+        EXTRACT(YEAR_MONTH FROM timestamp) AS month,
+        SUM(1) AS count
+
+    FROM {{ tables.edits_web }} WHERE
+        event_action = 'success' AND
+        event_namespace = 0 AND
+        wiki != 'testwiki'
+    GROUP BY month
+) AS Main ON Month.month = Main.month
+LEFT JOIN (
+    SELECT
+        EXTRACT(YEAR_MONTH FROM timestamp) AS month,
+        SUM(1) AS count
+
+    FROM {{ tables.edits_web }} WHERE
+        event_action = 'success' AND
+        event_namespace != 0 AND
+        wiki != 'testwiki'
+    GROUP BY month
+) AS Other ON Month.month = Other.month;
+
