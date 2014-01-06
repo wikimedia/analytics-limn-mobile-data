b7f9f8db4d7cc6acb82d073ce6f93c3ce94abd3d
jrobson <jrobson@wikimedia.org>
2014-01-06 14:41:44 -0800

    Story 1481: Collect graph data only for current month
    For older months when available resort to the existing CSV file
    rather than rewriting
    Currently only applied to edits-monthly-5plus-editors
    
    Change-Id: Iac4d1b93304228cface53948c7d0ca2ba9a707b3

--- generate.py
+++ generate.py
@@ -12,6 +12,8 @@ import jinja2
 import yaml
 
 import time
+import datetime
+from dateutil.relativedelta import relativedelta
 import json
 
 LOG_FILE = 'history.json'
@@ -67,14 +69,16 @@ class DataGenerator(object):
         t = jinja2.Template(template)
         return t.render(**self.config)
 
-    def execute_sql(self, file_name, db_name):
-        """Reads a query from `file_name`, renders it, and executes it against
-        a database, specified by configuration key.
+    def get_sql_string(self, file_name):
+        with io.open(file_name, encoding='utf-8') as f:
+            return self.render(f.read())
+
+    def execute_sql(self, sql, db_name):
+        """Executes a query `sql` against
+        a database `db_name`
 
         Returns a tuple of (headers, rows).
         """
-        with io.open(file_name, encoding='utf-8') as f:
-            sql = self.render(f.read())
 
         if self.debug_folder:
             debug_filename = os.path.join(self.debug_folder, os.path.basename(file_name))
@@ -142,8 +146,12 @@ class DataGenerator(object):
             due_at = last_run_time + increment
 
             if due_at < now or self.force:
+                if "timeboxed" in value and "starts" in value:
+                    from_date = value["starts"]
+                    self.generate_graph_timeboxed(key, value, from_date)
+                else:
+                    self.generate_graph_full( key, value )
                 try:
-                    self.generate_graph(key, value)
                     history[key] = now
                 except:
                     continue
@@ -153,23 +161,89 @@ class DataGenerator(object):
             else:
                 print('Skipping generation of {0}: not enough time has passed'.format(value['title']))
 
-    def generate_graph(self, key, value):
+    def generate_graph_timeboxed( self, graph_key, value, from_date ):
+        csv_filename = self.get_csv_filename(graph_key)
+        cache = {}
+        # load existing values from csv
+        csv_header = None
+        try:
+            with open(csv_filename, 'r') as csv_file:
+                reader = csv.reader(csv_file)
+                for row in reader:
+                    # skip header
+                    if csv_header:
+                        # store in cache
+                        cache[row[0]] = row[1:]
+                    else:
+                        csv_header = row
+        except IOError:
+            pass
+
+        today = datetime.date.today()
+        this_month = datetime.date(today.year, today.month, 1)
+        sql_path = self.get_sql_path(graph_key)
+        if os.path.exists(sql_path):
+            while from_date < this_month:
+                graph_date_key = from_date.strftime('%Y-%m-%d')
+                from_timestamp = from_date.strftime('%Y%m%d%H%M%S')
+                from_date = from_date + relativedelta(months=1)
+                to_timestamp = from_date.strftime('%Y%m%d%H%M%S')
+                # Generate a graph if not in cache or the current month
+                if graph_date_key not in cache or from_date >= this_month:
+                    print 'Generating data for month %s' % graph_date_key
+                    db_name = value.get('db', self.config['defaults']['db'])
+                    query = self.get_sql_string(sql_path)
+                    # apply timeboxing
+                    query = query.format(
+                        from_timestamp=from_timestamp,
+                        to_timestamp=to_timestamp
+                    )
+                    headers, rows = self.execute_sql(query, db_name)
+                    if not csv_header:
+                        csv_header = headers
+                        # FIXME: Support other time periods other than months?
+                        csv_header.insert(0, 'Month')
+                    cache[graph_date_key] = list(rows[0])
+                else:
+                    print 'Skip generation of %s' % graph_date_key
+
+
+            rows = []
+            for month, row in cache.iteritems():
+                row.insert(0, month)
+                rows.append(row)
+            if len(rows) > 0:
+                self.save_graph_as_csv(graph_key, csv_header, rows)
+            else:
+                print 'No data to write.'
+        else:
+            print 'Bad SQL given'
+
+    def generate_graph_full( self, key, value ):
             print('Generating {0}'.format(value['title']))
             # Look for the sql first, then python
             db_name = value.get('db', self.config['defaults']['db'])
 
-            if os.path.exists(os.path.join(self.folder, key + '.sql')):
-                file_path = os.path.join(self.folder, key + '.sql')
-                headers, rows = self.execute_sql(file_path, db_name)
+            sql_path = self.get_sql_path(key)
+            if os.path.exists(sql_path):
+                query = self.get_sql_string(sql_path)
+                headers, rows = self.execute_sql(query, db_name)
             elif os.path.exists(os.path.join(self.folder, key + '.py')):
                 file_path = os.path.join(self.folder, key + '.py')
                 headers, rows = self.execute_python(key, file_path)
             else:
                 raise ValueError('Can not find SQL or Python for {0}'.format(key))
+            self.save_graph_as_csv( key, headers, rows )
 
-            output_path = self.config['output']['path']
-            csv_filename = os.path.join(output_path, key + '.csv')
+    def get_sql_path( self, key ):
+        return os.path.join(self.folder, key + '.sql')
 
+    def get_csv_filename( self, key ):
+        output_path = self.config['output']['path']
+        return os.path.join(output_path, key + '.csv')
+
+    def save_graph_as_csv( self, key, headers, rows ):
+            csv_filename = self.get_csv_filename( key )
             with open(csv_filename, 'wb') as csv_file:
                 writer = csv.writer(csv_file)
                 writer.writerow(headers)
