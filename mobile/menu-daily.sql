824541fa5b29969e57d3caf8737f92b3afed178a
jrobson <jrobson@wikimedia.org>
2014-01-29 15:06:03 -0800

    Add graph to track main menu clicks
    This shows us how popular/unpopular our features are in
    the left menu
    
    Change-Id: Ia4ed018886b921c8e3b954debfd0503c8e43df38

---
+++ mobile/menu-daily.sql
@@ -0,0 +1,14 @@
+select
+    sum(if(event_name = 'hamburger-home', 1, 0)) as Home,
+    sum(if(event_name = 'hamburger-random', 1, 0)) as Random,
+    sum(if(event_name = 'hamburger-uploads', 1, 0)) as Uploads,
+    sum(if(event_name = 'hamburger-watchlist', 1, 0)) as Watchlist,
+    sum(if(event_name = 'hamburger-nearby', 1, 0)) as Nearby,
+    sum(if(event_name = 'hamburger-login', 1, 0)) as Login,
+    sum(if(event_name = 'hamburger-logout', 1, 0)) as Logout,
+    sum(if(event_name = 'hamburger-profile', 1, 0)) as 'Profile (Beta only)'
+from
+    MobileWebClickTracking_5929948
+where
+    timestamp >= '{from_timestamp}' and
+    timestamp <= '{to_timestamp}'
