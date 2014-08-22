103e8622b1311af86dc98dd3fc96591b8b0839fb
milimetric <dandreescu@wikimedia.org>
2014-08-22 16:22:22 -0400

    Fix config and sql
    Change-Id: I3d46cc48d5827e513b4762f43b6f3fdfa190297f

--- mobile/app-edits-starts.sql
+++ mobile/app-edits-starts.sql
@@ -3,11 +3,11 @@ SELECT      COUNT( * ) AS Total,
             SUM( IF( userAgent LIKE '%Android%', 1, 0) ) AS Android,
             SUM( IF( userAgent LIKE '%Darwin%' OR userAgent LIKE '%iPhone OS%', 1, 0) ) AS iOS
 
-FROM        {{ tables.edits_app }} as Edits
+FROM        {{ tables.edits_app }}
 
 WHERE       event_action = 'start'
             AND wiki != 'testwiki'
-            AND Edits.timestamp >= '{from_timestamp}'
-            AND Edits.timestamp < '{to_timestamp}'
+            AND timestamp >= '{from_timestamp}'
+            AND timestamp < '{to_timestamp}'
 
 GROUP BY    DATE( timestamp )
