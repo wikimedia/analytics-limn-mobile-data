7e1f3698eb6617eba6263e3c306348cb63d8400e
bearND <bsitzmann@wikimedia.org>
2014-08-28 14:29:02 -0600

    Prepare for report: Number of blocked 'app users' on enwiki
    For this report: you are considered an 'app user' if
    you've created the account on the Android or iOS native app
    
    Change-Id: I2ecf7391055c503b47ff98b949a2b4bbef69c279

---
+++ mobile/app-user-blocks-en.sql
@@ -0,0 +1,23 @@
+-- Get number of blocked app users on enwiki
+-- For this report:
+-- you are considered an 'app user' if you've created the account on the Android or iOS native app
+
+SELECT DATE( ipb_timestamp)  AS Date,
+       COUNT( 1 ) AS NumBlocks
+FROM
+  (
+    SELECT ipb_user,
+           ipb_timestamp
+    FROM enwiki.ipblocks
+    WHERE ipb_timestamp >= '{from_timestamp}'
+      AND ipb_timestamp <= '{to_timestamp}'
+  ) blocks
+
+    -- only need ServerSideAccountCreation_5487345 and later since the earliest
+    -- account creation there was before the native apps were published
+  INNER JOIN ServerSideAccountCreation_5487345 ac
+          ON blocks.ipb_user = ac.event_userId
+
+WHERE ac.wiki = 'enwiki'
+
+GROUP BY DATE( ipb_timestamp )
