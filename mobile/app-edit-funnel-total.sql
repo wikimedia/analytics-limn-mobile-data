66faa57bd65568c96ca6892aed28cde85c6d6a7d
bearND <bsitzmann@wikimedia.org>
2014-08-28 14:58:08 -0600

    Add edit funnel sql for apps
    Similar to the previous app edits reports but one graph per OS
    with the main event_actions on the same graph.
    
    Am going to remove the previous ones once they get approved.
    Still want to keep them around for comparison sake.
    
    Change-Id: I2603c917cfc3c1379888f2c6e4c65c65d305c5f1

---
+++ mobile/app-edit-funnel-total.sql
@@ -0,0 +1,13 @@
+-- derived from cancelled-uploads.sql
+SELECT      SUM( IF( event_action = 'start', 1, 0) ) AS start,
+            SUM( IF( event_action = 'preview', 1, 0) ) AS preview,
+            SUM( IF( event_action = 'saveAttempt', 1, 0) ) AS saveAttempt,
+            SUM( IF( event_action = 'saved', 1, 0) ) AS saved
+
+FROM        {{ tables.edits_app }}
+
+WHERE       wiki != 'testwiki'
+            AND timestamp >= '{from_timestamp}'
+            AND timestamp < '{to_timestamp}'
+
+GROUP BY    DATE( timestamp )
