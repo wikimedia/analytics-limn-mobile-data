6145b74384189b1275e0a6ddd580ae626e732cdc
jdlrobson <jdlrobson@gmail.com>
2015-03-23 16:26:40 -0700

    Update limn graphs
    Bug: T93690
    Change-Id: I77f16666cb9cb1c0dc4fc19c26a1c314d82e9990

--- mobile/main-menu-daily.sql
+++ mobile/main-menu-daily.sql
@@ -7,7 +7,8 @@ SELECT
   sum(Settings) as Settings,
   sum(Profile) as Profile,
   sum(Logout) as Logout,
-  sum(Login) as Login
+  sum(Login) as Login,
+  sum(Collections) as Collections
 FROM (
   (
     SELECT
@@ -19,7 +20,8 @@ FROM (
       sum(if(event_name = 'hamburger-settings', 1, 0)) as Settings,
       sum(if(event_name = 'hamburger-profile', 1, 0)) as Profile,
       sum(if(event_name = 'hamburger-logout', 1, 0)) as Logout,
-      sum(if(event_name = 'hamburger-login', 1, 0)) as Login
+      sum(if(event_name = 'hamburger-login', 1, 0)) as Login,
+      0 as Collections
     FROM
       MobileWebClickTracking_5929948
 	WHERE
@@ -37,9 +39,29 @@ FROM (
       sum(if(event_name = 'settings', 1, 0)) as Settings,
       sum(if(event_name = 'profile', 1, 0)) as Profile,
       sum(if(event_name = 'logout', 1, 0)) as Logout,
-      sum(if(event_name = 'login', 1, 0)) as Login
+      sum(if(event_name = 'login', 1, 0)) as Login,
+      0 as Collections
     FROM
       MobileWebMainMenuClickTracking_10703095
+	WHERE
+	  timestamp >= '{from_timestamp}' AND
+	  timestamp <= '{to_timestamp}'
+  )
+  UNION
+  (
+    SELECT
+      sum(if(event_name = 'home', 1, 0)) as Home,
+      sum(if(event_name = 'random', 1, 0)) as Random,
+      sum(if(event_name = 'nearby', 1, 0)) as Nearby,
+      sum(if(event_name = 'watchlist', 1, 0)) as Watchlist,
+      sum(if(event_name = 'uploads', 1, 0)) as Uploads,
+      sum(if(event_name = 'settings', 1, 0)) as Settings,
+      sum(if(event_name = 'profile', 1, 0)) as Profile,
+      sum(if(event_name = 'logout', 1, 0)) as Logout,
+      sum(if(event_name = 'login', 1, 0)) as Login,
+      sum(if(event_name = 'collections', 1, 0)) as Collections
+    FROM
+      MobileWebMainMenuClickTracking_11568715
 	WHERE
 	  timestamp >= '{from_timestamp}' AND
 	  timestamp <= '{to_timestamp}'
